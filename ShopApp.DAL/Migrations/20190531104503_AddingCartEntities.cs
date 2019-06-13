f7e9c6c3dbeb9094afce114ac62d59b72fb5f681
Hilay <50641561+Hilaysonmez@users.noreply.github.com>
2019-06-13 22:25:52 +0300

    E-Ticaret Projesi
    MVC ASP.NET Core ile E-ticaret Web uygulaması

---
+++ ShopApp.DAL/Migrations/20190531104503_AddingCartEntities.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShopApp.DAL.Migrations
+{
+    public partial class AddingCartEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
